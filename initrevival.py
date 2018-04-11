5010d3c92100d5d0d5b0327b7b5d7cb35463c855
mistrjirka <jirka.svitil@gmail.com>
2018-04-11 20:32:42 +0200

    initrevival

--- initrevival.py
+++ initrevival.py
@@ -2,7 +2,7 @@ from ev3dev.ev3 import LargeMotor as LM
 from ev3dev.ev3 import MediumMotor as MM
 from ev3dev.ev3 import ColorSensor as CS
 from ev3dev.ev3 import TouchSensor as TS
-from time import sleep as waitFor
+from time import sleep as sleep
 
 class CacheManager:
 	def __init__(self):
@@ -27,17 +27,26 @@ class CacheManager:
 				self.cache[i] = {"key": i["key"], "data": data} 
 
 class Robot:
-	def __init__(self, commands = [{"direction":"right", "toDo": [-100, 100]}, {"direction":"left", "toDo": [100, -100]}, {"direction":"backward", "toDo": 600 * -1}, {"direction":"forward", "toDo": 600}], colorS = [{"val": [3,6], "toDo": "forward", "degreesDelay": 100}, {"val": [2], "toDo": "right", "degreesDelay": 100}, {"val": [5], "toDo": "left", "degreesDelay": 100}], motor1 = LM("outC"), motor2 = LM("outB")):
+	def __init__(self, commands = [{"direction":"right", "toDo": [-100, 100], "degreesDelay": 100}, {"direction":"left", "toDo": [100, -100], "degreesDelay": 100}, {"direction":"backward", "toDo": 600 * -1, "degreesDelay": 100}, {"direction":"forward", "toDo": 600, "degreesDelay": 100}}], colorS = [{"val": [3,6], "toDo": "forward", {"val": [2], "toDo": "right"}, {"val": [5], "toDo": "left"}], motor1 = LM("outC"), motor2 = LM("outB")):
 		self.commands = commands
 		self.motors = [motor1, motor2]
 		self.colorSheet = colorS
 		self.greenCounter = 0
 		self.colorBefore = None
 		self.doCache = cache.addToCache(None)
-	def do(self, whatToDo, mot1, mot2):
+	def do(self, whatToDo):
 		if whatToDo != cache.getFromCache(self.doCache):
 			for i in self.commands:
 				if i["direction"] == whatToDo:
+					self.motors[0].stop()
+					self.motors[1].stop()
+					self.dist = motors[0].position
+					self.motors[1].run_forever(speed_sp=i["toDo"])
+					self.motors[0].run_forever(speed_sp=i["toDo"])
+					for (motors[0].position - self.dist) < i["degreesDelay"]:
+						sleep(0.01)
+					self.motors[0].stop()
+					self.motors[1].stop()
 					if type(i["toDo"]).__name__ is "list":
 						self.motors[0].run_to_rel_pos(position_sp=i["toDo"][0], speed_sp=600, stop_action="hold")
 						self.motors[1].run_to_rel_pos(position_sp=i["toDo"][1], speed_sp=600, stop_action="hold")
@@ -61,7 +70,7 @@ class Robot:
 									self.colorBefore = color
 									return {"toDo": i["toDo"], "event": "brickDown"}
 						if self.colorBefore != color:
-							self.colorBefore = color				
+							self.colorBefore = color
 							return {"toDo": i["toDo"], "event": None}
 							
 						else:
@@ -69,6 +78,35 @@ class Robot:
 	def brickDownEvent(self, motor = MM("outD"), degrees = 180, speed = 1400, stopAction = "hold"):
 		motor.run_to_rel_pos(position_sp=degrees, speed_sp=speed, stop_action=stopAction)
 
+#motor init
+mot1 = LM("outC")
+mot2 = LM("outB")
+
 #class init
 cache = cacheManager()
-robot = Robot()
+robot = Robot(motor1=mot1, motor2=mot2)
+
+#sensor init
+cs = CS()
+ts = TS()
+
+#main loop
+def main():
+	mot1.run_forever(speed_sp = 700)
+	for cs.color = 4:
+		sleep(0.02)
+	while:
+		color = cs.color
+		toDo = robot.colorResponse(color)
+		if todo["event"] == None:
+			robot.do(toDo["toDo"])
+		else:
+			robot.brickDownEvent()
+			robot.do(toDo["toDo"])
+		sleep(0.1)
+
+#wait for signal to start
+while True:
+	if cs.color == 4 or ts.value() == 1:
+		main()
+		break
