038b78eafc68038f44e9d573b93408ba599968f3
mistrjirka <jirka.svitil@gmail.com>
2018-04-17 20:52:40 +0200

    motor calibration

--- initrevival.py
+++ initrevival.py
@@ -41,7 +41,7 @@ def getColorFromRaw(colorsnsr, colorResponse=[4,5,6,2,3], colorSheet=[[[160,290]
 		index = index + 1
 
 class Robot:
-	def __init__(self, commands = [{"direction":"right", "toDo": [-200, 200], "degreesDelay": 50}, {"direction":"left", "toDo": [200, -200], "degreesDelay": 50}, {"direction":"backward", "toDo": 500 * -1, "degreesDelay": 50}, {"direction":"forward", "toDo": 500, "degreesDelay": 50}], colorS = [{"val": [3,6], "toDo": "forward"}, {"val": [2], "toDo": "right"}, {"val": [5], "toDo": "left"}], motor1 = LM("outC"), motor2 = LM("outB")):
+	def __init__(self, commands = [{"direction":"right", "toDo": [-180, 180], "degreesDelay": 50}, {"direction":"left", "toDo": [180, -180], "degreesDelay": 50}, {"direction":"backward", "toDo": 500 * -1, "degreesDelay": 50}, {"direction":"forward", "toDo": 500, "degreesDelay": 50}], colorS = [{"val": [3,6], "toDo": "forward"}, {"val": [2], "toDo": "right"}, {"val": [5], "toDo": "left"}], motor1 = LM("outC"), motor2 = LM("outB")):
 		self.commands = commands
 		self.motors = [motor1, motor2]
 		self.colorSheet = colorS
